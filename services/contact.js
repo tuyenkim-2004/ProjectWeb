f489fbe82ef34c5644e01bd88fb18d894b3039fd
YMaiNgoc <ymaingoc629@gmail.com>
2024-10-18 10:41:20 +0700

    ngoc contact

--- services/contact.js
+++ services/contact.js
@@ -5,27 +5,22 @@ function subFunction() {
     const email = document.getElementById('email').value;
     const message = document.getElementById('form_control').value;
     const errorMessage = document.getElementById('error-message');
-  
+
     // Kiểm tra xem các trường đã được điền đầy đủ chưa
     if (!name || !phone || !email || !message) {
-      errorMessage.innerText = "Vui lòng điền đầy đủ thông tin vào các trường!";
-      return;
+        errorMessage.innerText = "Vui lòng điền đầy đủ thông tin!";
+        return;
+    } else {
+
     }
-  
-    // Kiểm tra email hợp lệ
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailPattern.test(email)) {
-      errorMessage.innerText = "Email không hợp lệ!";
-      return;
-    }
-  
-    // Sau khi kiểm tra hợp lệ, bạn có thể gửi dữ liệu hoặc hiển thị thông báo thành công
+
+    // Nếu tất cả các trường đã được điền, xóa thông báo lỗi
     errorMessage.innerText = ""; 
     alert("Tin nhắn của bạn đã được gửi thành công!");
-  
+
     // Xóa nội dung form sau khi gửi thành công
     document.getElementById('name').value = '';
     document.getElementById('number_phone').value = '';
     document.getElementById('email').value = '';
     document.getElementById('form_control').value = '';
-  }
+}
