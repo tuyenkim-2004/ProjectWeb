f00325db09fafb541b5d66106eafc790643d9cf6
CED-b2005 <duqingping2k5@gmail.com>
2024-10-23 01:13:50 +0700

    1:30 23/10/2024 change code success

--- services/shoppingCart.js
+++ services/shoppingCart.js
@@ -9,54 +9,16 @@ var products = getLocalStorage()
 
 showProduct()
 
-<<<<<<< HEAD
 setInterval (()=> {
     products = getLocalStorage()
     productInfors = document.getElementsByClassName('productInfors')
     removeButtons = document.getElementsByClassName('remove')
     giohang()
-=======
+
     if (localStorage.length == 0) {
         buy.style.display = 'none'
         tbody.innerHTML = ` <div class="container" id="none" style=" textDecoration: none; font-size: 20px; color: #a05139"> <b>Giỏ Hàng Trống !!!</b> - <a href = '/projectWeb/page/home/home.html' style="textDecoration: none;">đi mua hàng</a></div>`
     }
-    else {
-        tbody.innerHTML = ""
-        buy.style.display = 'flex'
-        for (let i = 0; i < localStorage.length; i++) {
-            let key = localStorage.key(i)
-            fetch(`http://localhost:3000/products/${JSON.parse(localStorage.key(i))}`)
-                .then(Response => Response.json())
-                .then(product=> {
-                    const tr = document.createElement('tr')
-                    tr.innerHTML = `
-                  <td class="productInfor">
-                      <div class="container">
-                          <div class="container col-1">
-                              <input type="checkbox" class="checkbox">
-                          </div> 
-                              <img src= ${product.image}>
-                          <div class="container">
-                              <div>
-                                  <p class="productName">${product.name}</p>
-                                  <div class = 'change'>
-                                  <button class = 'reduce'  name = "${product.id}"> - </button>
-                                  <button class = 'increase'  name = "${product.id}"> + </button>
-                                  <button class = 'delete'  name = "${product.id}"> xóa </button>
-                                  </div>
-                              </div>
-                          </div>        
-                      </div>
-                  </td>
-                  <td class="productPrice">${product.price}</td>
-                  <td class="productQuan">${JSON.parse(localStorage.getItem(key))[1]}</td>
-                  <td class="productTotalPrice">${product.price * JSON.parse(localStorage.getItem(key))[1]}</td>        `
-                tbody.appendChild(tr)
-            })
-        }
-    }
->>>>>>> a36062befe36f5bbb61fb783f4c815800146a5ef
-
     showProduct()
 
     setCheckbox()
@@ -67,7 +29,7 @@ setInterval (()=> {
     addOrSubQuanyity('sub')
 
     removeOnShow()
-},500)
+},600)
 
 
 function removeOnShow ()  {
@@ -197,7 +159,3 @@ function setCheckbox () {
 window.addEventListener('dblclick', function () {
     alert('Bạn thao tác quá nhanh! Xin vui lòng chậm lại! Cảm Ơn')
 })
-
-// function format (value)  {
-//     for (value.length) {}
-// }
